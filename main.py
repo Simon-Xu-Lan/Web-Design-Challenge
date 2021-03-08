1e20cbc0d442a32627232c60d4082d5d2835b63d
Simon Xu <xulan20907@gmail.com>
2021-03-08 10:56:23 -0700

    Add requirements.txxt and Procfile

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import pandas as pd
 df = pd.read_csv("./static/assets/data/cities.csv")
 with open("./templates/table.html", 'w') as file:
     file.write(df.to_html())
+cols = df.columns
+for row in df.head(1).iterrows():
+    print(type(row))
 
 app = Flask(__name__)
 
